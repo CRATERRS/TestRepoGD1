5a332fcd1509158d406648c9ee9d59761bf33308
Craters <adryan.casinenau99@gmail.com>
2023-02-15 18:01:48 +0100

    le

--- Assets/Scrpts/PlayerController.cs
+++ Assets/Scrpts/PlayerController.cs
@@ -4,23 +4,81 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]float speed = 5f;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpHeight = 5f;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] LayerMask groundMask;
 
+    public int score = 0;
+
+    bool isGrounded = false;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxis("Horizontal");
+        //Polling input
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
 
-        Vector3 playerMovement = Vector3.right * xMove * speed * Time.deltaTime; 
-        transform.position += playerMovement;
+        //Costruisco il vettore movimento
+        Vector3 playerMovement = (Vector3.right * xMove + Vector3.forward * zMove).normalized * speed /** Time.deltaTime*/;
+
+        //Applico la mia velocity yverticale al vetore movimento
+        playerMovement.y = rb.velocity.y;
+
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerMovement.y += Mathf.Sqrt(jumpHeight * -2f * (-9.81f));
+        }
+
+
+        //Applico il vettore movimento al rigidbody
+        rb.velocity = playerMovement;
+
+
+
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.cyan);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, 10, groundMask));
+        {
+            Debug.Log("Colpito");
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Collectible"))
+        {
+            score++;
+            Debug.Log(score);
+            Destroy(other.gameObject);
+        }
+
+        if (other.transform.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
